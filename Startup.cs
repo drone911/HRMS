ee22d4201cc0366837cadadc75c9f226c43bd498
Drone911 <jigar1822@gmail.com>
2020-09-15 20:46:45 +0530

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRMSProj.Startup))]
+namespace HRMSProj
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

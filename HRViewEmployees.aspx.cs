0c22b52f0f98bbff56ba4b61b09c74a80f66bc94
Drone911 <jigar1822@gmail.com>
2020-11-06 15:30:15 +0530

    After Removing Dev Express

--- HRViewEmployees.aspx.cs
+++ HRViewEmployees.aspx.cs
@@ -9,7 +9,6 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using DevExpress.Web;
 public partial class HRViewEmployees : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
